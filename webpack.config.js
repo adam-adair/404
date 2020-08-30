848f04cd35626378bd4c3459a86107d6a698dc4a
Jeff <garber34@gmail.com>
2020-08-30 16:23:52 -0400

    terser plugin optimization included in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const Packer = require('packer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: "production",
@@ -6,15 +7,21 @@ module.exports = {
     fs: 'empty',
     net:'empty'
   },
+  ///// using the terser plugin
+  // optimization: {
+  //   minimize: true,
+  //   minimizer: [new TerserPlugin()],
+  // },
   entry: './app.js', // the starting point for our program
   output: {
     path: __dirname, // the absolute path for the directory where we want the output to be placed
     filename: 'bundle.js' // the name of the file that will contain our output - we could name this whatever we want, but bundle.js is typical
-  },
+  }
+  ,
   plugins: [
     new Packer({ // Must be initialized per bundle
       blacklist: [/aws-sdk/], // Optional
       packageManager: 'npm'   // Optional
     }),
   ],
-}
+ }
